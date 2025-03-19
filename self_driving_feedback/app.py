2fcd7ebac9c61c5f8ac03e8fe18c77f49919f4a7
Collegenie-X <148940286+Collegenie-X@users.noreply.github.com>
2025-03-19 09:49:18 +0900

    updated app

--- self_driving_feedback/app.py
+++ self_driving_feedback/app.py
@@ -3,6 +3,13 @@ from survey_part import show_survey
 from result_part import show_result
 
 
+def display_header(key_value):
+    _, col2 = st.columns([7, 1])
+
+    with col2:
+        st.button("내보내기", key=f"export_{key_value}")
+
+
 def main():
     st.set_page_config(page_title="자율주행 설문/결과", layout="centered")
 
@@ -10,7 +17,7 @@ def main():
         """
         <style>
         .block-container {
-            max-width: 1100px; /* 세미콜론(;) */
+            max-width: 1200px; /* 세미콜론(;) */
             margin: 0 auto;   /* 가운데 정렬 */
         }
         </style>
@@ -21,31 +28,12 @@ def main():
     # ✅ 탭 메뉴 생성
     tabs = st.tabs(["설문 부분", "결과 부분"])
 
-    with tabs[0]:
-        # 미리보기, 내보내기 버튼 (위치 조정 가능)
-
-        col1, col2, col3 = st.columns([7, 1, 1])
-
-        with col2:
-            st.button("미리보기", key="preview_survey")
-
-        with col3:
-            st.button("내보내기", key="export_survey")
-
-        # 설문 부분
+    with tabs[0]:  ## 설문 부분
+        display_header("survey")
         show_survey()
 
-    with tabs[1]:
-
-        col1, col2, col3 = st.columns([7, 1, 1])
-
-        with col2:
-            st.button("미리보기", key="preview_result")
-
-        with col3:
-            st.button("내보내기", key="export_result")
-
-        # 결과 부분
+    with tabs[1]:  ## 결과 부분
+        display_header("result")
         show_result()
 
 
