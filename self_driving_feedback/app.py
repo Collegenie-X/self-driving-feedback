2d093ed4704c9934495206463dd57247d79faf15
Delt-galeon <tlawodbs10@naver.com>
2025-03-19 10:06:39 +0900

    Update app.py

--- self_driving_feedback/app.py
+++ self_driving_feedback/app.py
@@ -10,7 +10,7 @@ def main():
         """
         <style>
         .block-container {
-            max-width: 1100px; /* 세미콜론(;) */
+            max-width: 1200px; /* 세미콜론(;) */
             margin: 0 auto;   /* 가운데 정렬 */
         }
         </style>
@@ -21,13 +21,12 @@ def main():
     # ✅ 탭 메뉴 생성
     tabs = st.tabs(["설문 부분", "결과 부분"])
 
-    with tabs[0]:
+    with tabs[0]: # 설문 부분
         # 미리보기, 내보내기 버튼 (위치 조정 가능)
-
         col1, col2, col3 = st.columns([7, 1, 1])
 
-        with col2:
-            st.button("미리보기", key="preview_survey")
+        # with col2:
+        #     st.button("미리보기", key="preview_survey")
 
         with col3:
             st.button("내보내기", key="export_survey")
@@ -35,12 +34,13 @@ def main():
         # 설문 부분
         show_survey()
 
-    with tabs[1]:
+    with tabs[1]: # 결과 부분
+        # 미리보기, 내보내기 버튼 (위치 조정 가능)
 
         col1, col2, col3 = st.columns([7, 1, 1])
 
-        with col2:
-            st.button("미리보기", key="preview_result")
+        # with col2:
+        #     st.button("미리보기", key="preview_result")
 
         with col3:
             st.button("내보내기", key="export_result")
